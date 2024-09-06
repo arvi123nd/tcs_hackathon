895082338b32184151cbafb605a22666318e889f
Arvind Sheoram <arvind.sheoram@klm.com>
2024-09-07 01:27:07 +0200

    ..

--- app/static/script.js
+++ app/static/script.js
@@ -5,7 +5,7 @@ async function sendQuery() {
     console.log("Sending query:", query); // Debugging line
 
     try {
-        const response = await fetch('http://127.0.0.1:8001/generate', {
+        const response = await fetch('http://127.0.0.1:8000/generate', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
